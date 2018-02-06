20a00094330fe6f03d4fc4364c5c2e2e66046eeb
Arne Van Raepenbusch <arvraepe@gmail.com>
2018-02-06 11:58:40 +0100

    typooo

--- dm-core/src/models/base/BaseModel.js
+++ dm-core/src/models/base/BaseModel.js
@@ -1,4 +1,4 @@
 const DBHelper = require('helpers/DBHelper');
 const StringHelper = require('helpers/StringHelper');
 
-module.exports = DBHelper.model(config.name, require(`models/schemas/${config.name}Schema`));
+module.exports = (config) => DBHelper.model(config.name, require(`models/schemas/${config.name}Schema`));

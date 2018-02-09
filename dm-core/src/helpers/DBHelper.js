10de2818354f9899bd8f8c0c04063c2648d8a655
Dennis Schepers <dennis.schepers@jstack.eu>
2018-02-09 15:35:21 +0100

    Fixed full text search

--- dm-core/src/helpers/DBHelper.js
+++ dm-core/src/helpers/DBHelper.js
@@ -2,8 +2,9 @@ const Mongoose = require('mongoose');
 const model = (name, schema, options = { versionKey: false, minimize: false }) => {
   const mSchema = new Mongoose.Schema(schema, options);
 
+  console.log(mSchema);
   // Necessary for full text search
-  mSchema.index({ content: 'text' });
+  mSchema.index({'$**': 'text'});
 
   return Mongoose.model(name, mSchema);
 };

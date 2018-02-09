e8042e1fb6e096901fdf6c7c4718bb829f8cc883
Dennis Schepers <dennis.schepers@jstack.eu>
2018-02-09 15:38:58 +0100

    remove log

--- dm-core/src/helpers/DBHelper.js
+++ dm-core/src/helpers/DBHelper.js
@@ -2,7 +2,6 @@ const Mongoose = require('mongoose');
 const model = (name, schema, options = { versionKey: false, minimize: false }) => {
   const mSchema = new Mongoose.Schema(schema, options);
 
-  console.log(mSchema);
   // Necessary for full text search
   mSchema.index({'$**': 'text'});
 

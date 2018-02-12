ee6f6aa3e620d8ca3536f2f1e0ca04ef17336e31
Arne Van Raepenbusch <arvraepe@gmail.com>
2018-02-12 11:25:27 +0100

    added tags for shops and other entities

---
+++ dm-core/src/domain/configurations/Tag.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: 'Tag',
+  resource: 'tags'
+};
---
+++ dm-core/src/models/schemas/TagSchema.js
@@ -0,0 +1,9 @@
+const AuditSchema = require('models/schemas/AuditSchema');
+module.exports = R.mergeAll([
+  AuditSchema,
+  {
+    "name": { type: String, unique: true },
+    "entity": String,
+    "where": Object
+  }
+]);

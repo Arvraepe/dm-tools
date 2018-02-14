393fb7aa8706ffc68ed6368b5c23f830c6068b07
Sergej Gureev <bemyak@gmail.com>
2018-02-14 21:36:34 +0300

    Small fix, so that build is working

--- dm-ui/src/app/authentication/services/authentication.service.ts
+++ dm-ui/src/app/authentication/services/authentication.service.ts
@@ -1,8 +1,8 @@
 import {Injectable, EventEmitter} from '@angular/core';
 import {BehaviorSubject} from '../../../../../dm-core/node_modules/rxjs/BehaviorSubject.d';
 import {Observable} from '../../../../../dm-core/node_modules/rxjs/Observable.d';
-import {HttpClient} from "@angular/common/http";
-import {map} from '../../../../node_modules/rxjs/operators.d';
+import {HttpClient} from '@angular/common/http';
+import {map} from '../../../../../dm-core/node_modules/rxjs/operators.d';
 import {environment} from '../../../environments/environment';
 
 const toJson = (response) => response.json();

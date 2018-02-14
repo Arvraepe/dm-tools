f66463831f73ce43173fa93e983c4a720a1126c9
Arne Van Raepenbusch <arvraepe@gmail.com>
2018-02-14 09:09:34 +0100

    Started working on spells

--- dm-ui/src/app/entities/components/entity/spells/spell.component.ts
+++ dm-ui/src/app/entities/components/entity/spells/spell.component.ts
@@ -5,7 +5,26 @@ import {DynamicEntityComponent} from "../dynamic-entity.component";
 @Component({
     selector: 'dm-spell',
     template: `
-       <h1 class="title" *ngIf="data">{{ data.name }}</h1>
+        <div class="box">
+            <h1 class="title" *ngIf="data">{{ data.name }}</h1>
+            <p>
+                <i>{{ getLevel(data.level) }} ({{ getSchool(data.school) }})</i>
+            </p>
+            <p class="deemphasized">
+                Components:
+                <span *ngIf="data.components.v">V </span>
+                <span *ngIf="data.components.s">S </span>
+                <span *ngIf="data.components.m">M ({{ data.components.m }})</span>
+            </p>
+
+            <p style="margin-top: 12px;">
+                <span style="margin-right: 4px;" *ngFor="let class of getClasses(data)" class="tag">{{ class.name }}</span>
+            </p>
+
+            <p style="margin-top: 12px;" *ngFor="let entry of data.entries">
+                {{ entry }}
+            </p>
+        </div>
     `,
     styleUrls: ['./spell.scss']
 })
@@ -17,6 +36,30 @@ export class SpellComponent implements DynamicEntityComponent {
 
     ) {
 
+    }
+
+    getLevel (level) {
+        return level === 0 ? `Cantrip` : `Level ${level}`;
+    }
+
+    getSchool (school) {
+        return {
+            'C': 'Conjuration',
+            'A': 'Abjuration',
+            'D': 'Divination',
+            'E': 'Enchantment',
+            'V': 'Evocation',
+            'T': 'Transmuation',
+            'N': 'Necromancy'
+        }[school];
+    }
+
+    getClasses (data) {
+
+        const fromClassList = data.classes.fromClassList || [];
+        const fromSubClass = data.classes.fromSubClass || [];
+
+        return fromClassList.concat(fromSubClass);
     }
 
 }

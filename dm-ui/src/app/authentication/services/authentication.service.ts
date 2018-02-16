f8decf38de80ec60800b1a017bd84993172bbfab
Arninja <arvraepe@gmail.com>
2018-02-16 15:46:12 +0100

    Fix build

--- dm-ui/src/app/authentication/services/authentication.service.ts
+++ dm-ui/src/app/authentication/services/authentication.service.ts
@@ -1,6 +1,4 @@
 import {Injectable, EventEmitter} from '@angular/core';
-import {BehaviorSubject} from '../../../../../dm-core/node_modules/rxjs/BehaviorSubject.d';
-import {Observable} from '../../../../../dm-core/node_modules/rxjs/Observable.d';
 import {HttpClient} from '@angular/common/http';
 import {map} from '../../../../../dm-core/node_modules/rxjs/operators.d';
 import {environment} from '../../../environments/environment';

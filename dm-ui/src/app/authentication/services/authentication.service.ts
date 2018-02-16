17cfbcc9a49ea3985adf83f80b1fe88e39597ebd
Arninja <arvraepe@gmail.com>
2018-02-16 15:47:18 +0100

    Fix build

--- dm-ui/src/app/authentication/services/authentication.service.ts
+++ dm-ui/src/app/authentication/services/authentication.service.ts
@@ -1,7 +1,5 @@
 import {Injectable, EventEmitter} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map} from '../../../../../dm-core/node_modules/rxjs/operators.d';
-import {environment} from '../../../environments/environment';
 
 const toJson = (response) => response.json();
 
